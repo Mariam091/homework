2b6f0a0e3650b9caab2e6a6a2ab85350ea92b166
Mariam091 <138693487+Mariam091@users.noreply.github.com>
2023-08-28 16:19:10 +0400

    Create homework.cc

---
+++ homework.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     float x=0;
+     float y=0;
+     float result=0;
+
+     printf ("Please enter two numbers\n");
+     scanf ("%f %f", &x, &y);
+
+      if ((x+y) != 0) {
+        result =(abs(x-y))/(x+y);
+
+       printf ("%f\n", result);
+
+      }
+       else
+            printf ("sxal e");
+
+
+   }
