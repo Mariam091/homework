cd2d7954c279f47b3395f5bb8681c6f4f4bcfcd0
Mariam091 <138693487+Mariam091@users.noreply.github.com>
2023-08-28 16:50:37 +0400

    Update homework.c

--- homework.c
+++ homework.c
@@ -1,24 +1,17 @@
+//Write a program that generates and prints 50 random integers, each between 3 and 6.
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-     float x=0;
-     float y=0;
-     float result=0;
+    int  x=3;
+    int y=6;
 
-     printf ("Please enter two numbers\n");
-     scanf ("%f %f", &x, &y);
+    for (int i=0; i<50; i++)
+    {
+        int result = (rand()%(y-x+1))+x;
+          printf ("%d\n", result);
+        }
+    }
 
-      if ((x+y) != 0) {
-        result =(abs(x-y))/(x+y);
-
-       printf ("%f\n", result);
-
-      }
-       else
-            printf ("sxal e");
-
-
-   }
 
